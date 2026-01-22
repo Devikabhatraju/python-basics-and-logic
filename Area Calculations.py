f049ba33599abf86e6ee10532d69425546794cb0
Devikabhatraju <devikabhatraju6@gmail.com>
2026-01-22 16:57:11 +0530

    Add files via upload

---
+++ Area Calculations.py
@@ -0,0 +1,10 @@
+# Area of Circle
+r = float(input("Enter radius: "))
+circle_area = 3.14 * r * r
+print("Area of Circle:", circle_area)
+
+# Area of Rectangle
+l = float(input("Enter length: "))
+b = float(input("Enter breadth: "))
+rect_area = l * b
+print("Area of Rectangle:", rect_area)
